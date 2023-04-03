6c7b6cdf0edfc342c5a15ffff0996f081689a310
nidhiG30 <gadgenidhi9@gmail.com>
2023-04-03 20:31:58 +0530

    Imported icons for the app from REACT ICONS library.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { MdMic } from 'react-icons/md';
+import { HiOutlineBars3, HiMagnifyingGlass } from 'react-icons/hi2'; // HeroIcons2
+import { BiVideoPlus } from 'react-icons/bi'; // BoxIcons
+import { FaRegBell } from 'react-icons/fa'; // Font Awesome 5
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="bg-yt-black h-14 flex items-center pl-4 pr-5 justify-between fixed w-full z-10">
+      Navbar
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
