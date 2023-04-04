83142ff7485d958157985ba2af84659c42f26c6c
nidhiG30 <gadgenidhi9@gmail.com>
2023-04-04 20:25:10 +0530

    Developed Sidebar.

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,8 +1,70 @@
-import React from 'react';
-import {SideBarItems} from '../static/data'
+import React, { useState } from 'react';
+import { SideBarItems } from '../static/data';
+import '../App.css';
 
 const Sidebar = () => {
-  return <div className='w-60 bg-yt-black h-[calc(100vh-53px)] mt-14 fixed top-0 left-0 text-yt-white p-3 overflow-scroll'>Sidebar</div>
-}
+  const [active, setActive] = useState('Home');
+
+  return (
+    <div className="yt-scrollbar w-60 bg-yt-black h-[calc(100vh-53px)] mt-14 fixed top-0 left-0 text-yt-white p-3 overflow-scroll">
+      <div className="mb-4">
+        {SideBarItems.Top.map((item, index) => {
+          return (
+            <div
+              key={index}
+              className={`h-10 flex justify-start px-3 rounded-xl items-center cursor-pointer hover:bg-yt-light-black my-1 ${
+                item.name === active ? 'bg-yt-light-black' : 'bg-yt-black'
+              }`}
+              onClick={() => setActive(item.name)}
+            >
+              <span className="mr-5">{item.icon}</span>
+              <p className="p-2 text-sm font-medium">{item.name}</p>
+            </div>
+          );
+        })}
+      </div>
+
+      <hr className="text-yt-light-black my-2" />
+
+      <div className="mb-4">
+        {SideBarItems.Middle.map((item, index) => {
+          return (
+            <div
+              key={index}
+              className={`h-10 flex justify-start px-3 rounded-xl items-center cursor-pointer hover:bg-yt-light-black my-1 ${
+                item.name === active ? 'bg-yt-light-black' : 'bg-yt-black'
+              }`}
+              onClick={() => setActive(item.name)}
+            >
+              <span className="mr-5">{item.icon}</span>
+              <p className="p-2 text-sm font-medium">{item.name}</p>
+            </div>
+          );
+        })}
+      </div>
+
+      <hr className="text-yt-light-black my-2" />
+
+      <h2 className="px-3 pt-1">Explore</h2>
+
+      <div className="mb-4">
+        {SideBarItems.Explore.map((item, index) => {
+          return (
+            <div
+              key={index}
+              className={`h-10 flex justify-start px-3 rounded-xl items-center cursor-pointer hover:bg-yt-light-black my-1 ${
+                item.name === active ? 'bg-yt-light-black' : 'bg-yt-black'
+              }`}
+              onClick={() => setActive(item.name)}
+            >
+              <span className="mr-5">{item.icon}</span>
+              <p className="p-2 text-sm font-medium">{item.name}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default Sidebar;
