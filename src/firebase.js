1393bb93bdb95a869bba614b137a42bbeaebb8b9
nidhiG30 <gadgenidhi9@gmail.com>
2023-04-05 01:02:40 +0530

    Installed Firebase for Authentication & Database, used as Backend-as-a-Service.

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBx23MfynMMeiUMCrwlb8CPtVQTvSq-vLc",
+  authDomain: "clone-e8afe.firebaseapp.com",
+  projectId: "clone-e8afe",
+  storageBucket: "clone-e8afe.appspot.com",
+  messagingSenderId: "558753972236",
+  appId: "1:558753972236:web:673e66b3489e49a1f22e29"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
