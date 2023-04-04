abeea8c57d69d2f6c634f3d5cc6568c37115f771
nidhiG30 <gadgenidhi9@gmail.com>
2023-04-04 21:02:17 +0530

    Developed CategoryItems for most visited type of content on the app on Home Page. Styled the content and added a package to remove the scrollbar that was getting created.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import Sidebar from '../components/Sidebar'
+import { CategoryItems } from '../static/data'
 
 const Home = () => {
   return (
-    <div><Sidebar/></div>
+    <>
+      <Sidebar/>
+      <div className='w-[calc(100%-240px)] h-[calc(100%-53px)] pt-16 bg-yt-black ml-60'>
+        <div className='flex flex-row px-3 overflow-x-scroll relative scrollbar-hide'>{
+          CategoryItems.map((item, i) => (
+            <h2 className='text-yt-white font-normal text-sm py-2 px-4 break-keep whitespace-nowrap bg-yt-light my-3 cursor-pointer rounded-xl' key={i}>{item}</h2>
+          ))
+        }</div>
+      </div>
+    </>
   )
 }
 
