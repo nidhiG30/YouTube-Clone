5c5b8d228912ab5bed9a2da8c52229c9c5e4d967
nidhiG30 <gadgenidhi9@gmail.com>
2023-04-04 15:23:20 +0530

    Created Sidebar comp and imported it to Home page comp.

---
+++ src/components/Sidebar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {SideBarItems} from '../static/data'
+
+const Sidebar = () => {
+  return <div className='w-60 bg-yt-black h-[calc(100vh-53px)] mt-14 fixed top-0 left-0 text-yt-white p-3 overflow-scroll'>Sidebar</div>
+}
+
+export default Sidebar;
