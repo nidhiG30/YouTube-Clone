bb57d1712a44820c91077a048519c1469bc4d77f
nidhiG30 <gadgenidhi9@gmail.com>
2023-04-04 12:27:24 +0530

    Developed Search Bar and Search Button, along with Mic search and a button to Create Videos.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import { MdMic } from 'react-icons/md';
 import { HiOutlineBars3, HiMagnifyingGlass } from 'react-icons/hi2'; // HeroIcons2
 import { BiVideoPlus } from 'react-icons/bi'; // BoxIcons
 import { FaRegBell } from 'react-icons/fa'; // Font Awesome 5
-import logo from '../assets/yt-logo-white.png'
+import logo from '../assets/yt-logo-white.png';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
@@ -13,14 +13,41 @@ const Navbar = () => {
         <div className="text-yt-white p-2 w-10 text-2xl text-center hover:bg-yt-light-black rounded-full cursor-pointer">
           <HiOutlineBars3 />
         </div>
-        <div className='py-5 w-28 pr-3'>
+        <div className="py-5 w-24 pr-3">
           <Link to="/">
-            <img src={logo} alt="" className='object-contain' />
+            <img src={logo} alt="" className="object-contain" />
           </Link>
         </div>
       </div>
+
+      <div className="h-10 flex flex-row items-center flex-1">
+        <div className="w-[593px] bg-yt-black flex border border-yt-light-black items-center rounded-3xl h-10">
+          <input
+            type="text"
+            placeholder="Search"
+            className="w-full bg-yt-black h-6 ml-6 text-yt-white focus:outline-none pl-4"
+          />
+          <button className="w-16 h-10 bg-yt-light-black px-2 py-0.5 rounded-r-3xl border-yt-light-black">
+            <HiMagnifyingGlass
+              size={22}
+              className="text-yt-white inline-block text-center font-thin"
+            />
+          </button>
+        </div>
+
+        <div className="text-yt-white bg-yt-light w-10 h-10 items-center flex justify-center rounded-full ml-4 hover:bg-yt-light-black cursor-pointer">
+          <MdMic className="text-center" size={23} />
+        </div>
+      </div>
+
+      <div className="flex items-center justify-center">
+        <div className="flex flex-row items-center">
+          <div className="mr-2 p-2 w-10 hover:bg-yt-light-black rounded-full cursor-pointer">
+            <BiVideoPlus size={25} className="text-yt-white text-center" />
+          </div>
+        </div>
+      </div>
     </div>
-    
   );
 };
 
